7b59d0a996dcf06e7b10ee3d69d3b2cf2a70ebd4
Nick Torres <nickrtorres@icloud.com>
2021-02-24 23:24:38 -0800

    add lab 4 solutions

--- assignments/04/ListOps.fs
+++ assignments/04/ListOps.fs
@@ -18,19 +18,44 @@ type Account =
 // count x coll
 //
 // count the number of values equal to x in coll.
-
+//
+// val count: 'a -> 'a list -> int
+let rec count x coll =
+    match coll with
+    | [] -> 0
+    | hd :: tl when hd = x -> 1 + count x tl
+    | _ :: tl -> count x tl
 
 // countEvens coll
 //
 // count the number of even integers in coll.
-
+//
+// val countEvens: int list -> int
+let rec countEvens coll =
+    match coll with
+    | [] -> 0
+    | hd :: tl when hd % 2 = 0 -> 1 + countEvens tl
+    | _ :: tl -> countEvens tl
 
 // lastElement coll
 //
 // return the last element in the list
-
+//
+// val lastElement: 'a list -> 'a
+let rec lastElement coll =
+    match coll with
+    | [] -> failwith "list is empty!"
+    | [ x ] -> x
+    | _ :: tl -> lastElement tl
 
 // maxOverdrawn coll
 //
 // given a list of Accounts, return the largest Overdrawn amount, or 0 if none
 // are overdrawn
+//
+// val maxOverdrawn: Account list -> int
+let rec maxOverdrawn coll =
+    match coll with
+    | [] -> 0
+    | Overdrawn amount :: tl -> max amount (maxOverdrawn tl)
+    | _ :: tl -> maxOverdrawn tl
