4d10f424b112c6527bb6cc56fde34c98608be281
Nick Torres <nickrtorres@icloud.com>
2021-03-06 12:02:03 -0800

    add lab 6 solutions

--- assignments/06/ListOps.fs
+++ assignments/06/ListOps.fs
@@ -19,3 +19,31 @@ let makeCustomerWithBalance name (amount: int) =
     { Name = name; Account = account }
 
 let unknownCustomer = makeCustomerWithBalance "Unknown" 0
+
+let totalOverdrawn name customers =
+    let netOverdrawn =
+        function
+        | { Name = _; Account = Overdrawn n } -> n
+        | _ -> 0
+
+    if List.isEmpty customers then
+        0
+    else
+        List.filter (fun e -> e.Name = name) customers
+        |> List.map netOverdrawn
+        |> List.reduce (+)
+
+let maxBalance name customers =
+    let filterCustomer =
+        function
+        | { Name = n; Account = Balance _ } when n = name -> true
+        | _ -> false
+
+    let customerReduction acc cust =
+        match (acc.Account, cust.Account) with
+        | Balance bLeft, Balance bRight when bLeft < bRight -> cust
+        | _ -> acc
+
+    match List.filter filterCustomer customers with
+    | [] -> unknownCustomer
+    | customers -> List.reduce customerReduction customers
