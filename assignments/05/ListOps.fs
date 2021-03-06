e37a6bd277ceb6ca8bf4c810525e5ebcc3393ec1
Nick Torres <nickrtorres@icloud.com>
2021-03-06 12:00:23 -0800

    add lab 5 solution

--- assignments/05/ListOps.fs
+++ assignments/05/ListOps.fs
@@ -1,1 +1,9 @@
 module ListOps
+
+let suffixes coll =
+    let rec suffixes' acc coll =
+        match coll with
+        | [] -> [] :: acc
+        | _ :: tl -> suffixes' (coll :: acc) tl
+
+    suffixes' [] coll |> List.rev
