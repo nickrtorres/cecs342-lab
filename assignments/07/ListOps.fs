1576dba561b67eea0f815344258764eb08f60f58
Nick Torres <nickrtorres@icloud.com>
2021-03-17 10:16:56 -0700

    add lab 7 solutions

--- assignments/07/ListOps.fs
+++ assignments/07/ListOps.fs
@@ -22,3 +22,64 @@ let deposit amount acc =
     | Overdrawn n when n - amount > 0 -> Overdrawn(n - amount)
     | Overdrawn n when n - amount < 0 -> Balance(abs (n - amount))
     | Overdrawn _ -> Empty
+
+// fun acc elem -> <combine the current element with the acc somehow>
+//
+//     using empty string as starting point
+//     ------------------------------------
+//     expected value = "this is text"
+//     coll = ["this"; "is"; "text"]
+//     delim = " "
+//
+//     acc         apply function
+//     --------------------------
+//     ""          "" + " " + "this"         -> " this"
+//     " this"     " this" + " " + "is"      -> " this is"
+//     " this is"  " this is" + " " + "text" -> " this is text"[1:]
+//
+//     OR
+//
+//     using hd as the starting point
+//     ------------------------------
+//     expected value = "this is text"
+//     expected value = "this is text"
+//     coll = ["this"; "is"; "text"]
+//     delim = " "
+//
+//     acc         elem
+//     ----------------
+//     "this"      "this" + " " + "is"      -> "this is"
+//     "this is"   "this is" + " " + "text" -> "this is text"
+let join delim coll =
+    match coll with
+    | [] -> ""
+    | hd :: tl -> List.fold (fun acc elem -> acc + delim + elem) hd tl
+
+
+// The keys to successfully solving this problem:
+// - use the types
+// - use 'programming by wishful thinking'
+// - keep it simple
+let simplifyBank customers names =
+    // assumption: every customer in this list has the same name
+    let reduceCustomer customers =
+        let combineCustomers acc elem =
+            match elem.Account with
+            | Balance b ->
+                { acc with
+                      Account = deposit b acc.Account }
+            | Overdrawn o ->
+                { acc with
+                      Account = withdraw o acc.Account }
+            | Empty -> acc
+
+        List.reduce combineCustomers customers
+
+    // what must the type of simplifyCustomer be?
+    // Customer list -> string -> Customer list
+    let simplifyCustomer acc name =
+        (List.filter (fun c -> c.Name = name) customers
+         |> reduceCustomer)
+        :: acc
+
+    List.fold simplifyCustomer [] names |> List.rev
